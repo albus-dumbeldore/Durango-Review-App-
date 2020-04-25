acd3e6c4438f8957f768734fe53619d82f54f53d
albus-dumbeldore <dhruv0426@gmail.com>
2020-04-25 14:00:56 +0530

    port change

--- app.js
+++ app.js
@@ -31,7 +31,12 @@ var authRoutes        = require("./routes/auth.js")
 // mongoose.set('useNewUrlParser', true);
 // mongoose.set('useUnifiedTopology',true);
 // mongoose.connect("mongodb://localhost/manhattan4",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://review:yohoneysingh@1@cluster0-jhje6.mongodb.net/test?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+    
 
+});
 
 
 //============================================================================================================     
